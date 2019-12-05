c1d439791a69eec24fd42d7781d82d58fca4e599
maxsh <869209045@qq.com>
2019-12-05 22:40:40 +0800

    mybatis

---
+++ mybatis-xml/src/main/java/com/maxsh/MybatisXmlApplication.java
@@ -0,0 +1,13 @@
+package com.maxsh;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("com.maxsh.mapper")
+public class MybatisXmlApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisXmlApplication.class, args);
+    }
+}
---
+++ mybatis-xml/src/test/java/com/maxsh/MybatisXmlApplicationTests.java
@@ -0,0 +1,39 @@
+package com.maxsh;
+
+import com.maxsh.enums.UserSexEnum;
+import com.maxsh.mapper.UserMapper;
+import com.maxsh.model.User;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@SpringBootTest
+@DisplayName("测试UserMapper")
+class MybatisXmlApplicationTests {
+
+    @Resource
+    private UserMapper userMapper;
+
+    @Test
+    @DisplayName("测试User增删改查")
+    public void testUser()  {
+        User user = new User();
+        user.setName("张三");
+        user.setPassword("zs234");
+        user.setAge(18);
+        user.setSex(UserSexEnum.MAN);
+        //增加
+        userMapper.insert(user);
+        //删除
+        int  count =userMapper.delete(2l);
+        User user1  = userMapper.getOne(5l);
+        //修改
+        userMapper.update(user1);
+        //查询
+        List<User> users = userMapper.getAll();
+    }
+
+}
