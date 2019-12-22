adab57cd0a9ba498f56c099a4774379cc333c218
maxsh <869209045@qq.com>
2019-12-23 07:04:16 +0800

    druid

---
+++ druid-decrypt/src/main/java/com/maxsh/DruidDecryptApplication.java
@@ -0,0 +1,13 @@
+package com.maxsh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DruidDecryptApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DruidDecryptApplication.class, args);
+    }
+
+}
