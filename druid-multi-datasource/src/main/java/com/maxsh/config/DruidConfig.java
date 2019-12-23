27a0e02d29d5a6c047406ba00c2797ba280cba3f
maxsh <869209045@qq.com>
2019-12-23 17:01:38 +0800

    druid

---
+++ druid-multi-datasource/src/main/java/com/maxsh/config/DruidConfig.java
@@ -0,0 +1,16 @@
+package com.maxsh.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * DruidConfig
+ *
+ * @author maxsc
+ * @description: TODO
+ * @date 2019/12/23
+ */
+@Configuration
+public class DruidConfig {
+
+
+}
